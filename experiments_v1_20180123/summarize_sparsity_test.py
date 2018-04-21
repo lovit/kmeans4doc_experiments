af31a066f989ca228c3ef41109a679044f28f348
lovit <soy.lovit@gmail.com>
2018-04-22 01:41:24 +0900

    Add nonzero proportion of dense centroid

--- experiments_v1_20180123/summarize_sparsity_test.py
+++ experiments_v1_20180123/summarize_sparsity_test.py
@@ -12,6 +12,7 @@ output_path = 'sparsity_test_result.txt'
 
 from glob import glob
 import numpy as np
+import pickle
 
 with open(output_path, 'w', encoding='utf-8') as fo:
 
@@ -31,8 +32,14 @@ with open(output_path, 'w', encoding='utf-8') as fo:
             log_path = '/'.join(log_path.split('/')[:-1])+'/dense_sparse_center_difference.txt'
             diff = np.loadtxt(log_path)
             average_difference = diff.mean()
+            
+            center_path = '/'.join(log_path.split('/')[:-1])+'/cluster_centers_dense.pkl'
+            with open(center_path, 'rb') as f:
+                centers = pickle.load(f)
 
-            # percentage of nonzero elements, dense-sparse centroid difference
-            fo.write('{}\t{}\t{}\t{}\n'.format(data_index, spec, sparsity, average_difference))
+            p_nnz_of_dense = centers.nonzero()[0].shape[0] / (centers.shape[0] * centers.shape[1])
+
+            # percentage of nonzero elements, dense-sparse centroid difference, p_nnz_of_dense
+            fo.write('{}\t{}\t{}\t{}\t{}\n'.format(data_index, spec, sparsity, average_difference, p_nnz_of_dense))
 
         print('done with dataset = {}'.format(data_index))
